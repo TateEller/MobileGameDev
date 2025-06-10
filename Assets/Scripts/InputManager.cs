57e77d28b16314687a09b91efef82b3db48d775e
TateEller <tce22a@acu.edu>
2025-06-09 20:36:30 -0500

    test

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] private Camera sceneCamera;
+
+    private Vector3 lastPosition;
+
+    [SerializeField] private LayerMask placementLayermask;
+
+    public event Action OnClicked, OnExit;  //change to touch----------------------------------------
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            OnClicked?.Invoke();
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            OnExit?.Invoke();
+        }
+    }
+
+    public bool IsPointerOverUI() => EventSystem.current.IsPointerOverGameObject();
+
+    public Vector3 GetSelectedMapPosition()
+    {
+        //This needs to be updated for mobile and not use ouse input!!!!!!---------------------------------------------------------
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = sceneCamera.nearClipPlane;
+        Ray ray = sceneCamera.ScreenPointToRay(mousePos);
+
+        RaycastHit hit;
+        if(Physics.Raycast(ray,out hit, 100, placementLayermask))
+        {
+            lastPosition = hit.point;
+        }
+        return lastPosition;
+    }
+}
